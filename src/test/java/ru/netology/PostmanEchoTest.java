2131bdc053c9694f34b60ff25b2f57b2dafc77d5
Daniil Salikov <denielnit@gmail.com>
2026-01-13 17:07:40 +0300

    PostmnanEcho add

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldEchoPostedData() {
+        String sentData = "Привет! Как дела? :р";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(sentData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(sentData));
+    }
+}
